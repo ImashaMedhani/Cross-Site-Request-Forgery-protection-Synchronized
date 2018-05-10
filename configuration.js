cc5214055ea2e108bde80a13e4fb78b72a804e12
ImashaMedhani <39133862+ImashaMedhani@users.noreply.github.com>
2018-05-10 04:25:02 -0700

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,18 +1,14 @@
 var csrf_token;
 
-//use this function to retrive the response values coming form server side
 function loadDOC(method,url,htmlTag)
 {
-    var xhttp = new XMLHttpRequest(); //create variable for store HTTP requests
-    xhttp.onreadystatechange = function() //excute when receive an answer from server side
+    var xhttp = new XMLHttpRequest(); 
+    xhttp.onreadystatechange = function() 
     {
         if(this.readyState==4 && this.status==200)
         {
             console.log("CSRF token scuessfully fetched : "+this.responseText);
-            document.getElementById(htmlTag).value = this.responseText;
-            //return this.responseText; //return response
-            
-            
+            document.getElementById(htmlTag).value = this.responseText;   
         }
     };
 
